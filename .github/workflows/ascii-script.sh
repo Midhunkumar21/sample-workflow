85e6b7bd98bc7d73c0e09f32729a89c56b039c44
Midhunkumar P <147497248+Midhunkumar21@users.noreply.github.com>
2024-12-16 17:50:26 +0530

    general

--- .github/workflows/ascii-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get install cowsay -y
-cowsay -f dragon "run for cover, I am Dragon...RAWR" >> dragon.txt
-grep -i "dragon" dragon.txt
-cat dragon.txt
-ls
